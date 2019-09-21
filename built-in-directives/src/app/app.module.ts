a2401ad0a7135bbf119cf085c7888c8f59fc2163
Cesar Acual <chechaacual@gmail.com>
2019-09-21 08:57:24 -0600

    NgClass directive

--- built-in-directives/src/app/app.module.ts
+++ built-in-directives/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgSwitchExampleComponent } from "./ng-switch-example/ng-switch-example.component";
 import { NgStyleExampleComponent } from "./ng-style-example/ng-style-example.component";
+import { NgClassExampleComponent } from "./ng-class-example/ng-class-example.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NgSwitchExampleComponent,
-    NgStyleExampleComponent
+    NgStyleExampleComponent,
+    NgClassExampleComponent,
   ],
   imports: [
     BrowserModule
